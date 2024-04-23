5c0a11a99f022a34f9f8c08cb9859290b6113553
Jack Spinola <jackspinola198@hotmail.com>
2024-04-23 16:26:10 +0200

    animations for section1 &&  3

--- src/components/section1.js
+++ src/components/section1.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import '../css/section1.css';
-import '../css/button.css'
+import '../css/button.css';
 import partyImage from '../images/phone.png';
 
 function Section1() {
+  const leftColumnRef = useRef(null);
+  const rightColumnRef = useRef(null);
+
+  useEffect(() => {
+    const leftColumn = leftColumnRef.current;
+    const rightColumn = rightColumnRef.current;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            leftColumn.classList.add('slide-in-left');
+            rightColumn.classList.add('slide-in-right');
+          }
+        });
+      },
+      { threshold: 0.5 } // Adjust threshold as needed
+    );
+
+    observer.observe(leftColumn);
+    observer.observe(rightColumn);
+
+    return () => {
+      observer.unobserve(leftColumn);
+      observer.unobserve(rightColumn);
+    };
+  }, []);
+
   return (
     <div className="section d-flex align-items-center justify-content-center">
       <div className="row w-100">
-        <div className="col-md-6 d-flex align-items-center justify-content-center">
+        <div
+          ref={leftColumnRef}
+          className="col-md-6 d-flex align-items-center justify-content-center"
+        >
           <div className="w-90 h-100 d-flex align-items-center justify-content-center">
-            <div className='text-center w-50'>
-              <p className='title'>THE free PARTY APP</p>
-              <p className='subtitle w-100'>Get a free drink to start the night every time you explore with PNTA.</p>
+            <div className="text-center w-50">
+              <p className="title">THE free PARTY APP</p>
+              <p className="subtitle w-100">
+                Get a free drink to start the night every time you explore
+                with PNTA.
+              </p>
               <div className="d-flex justify-content-end">
                 <button className="funny-btn">Get App</button>
               </div>
             </div>
           </div>
         </div>
-        <div className="col-md-6 d-flex align-items-center justify-content-center overflow-hidden">
+        <div
+          ref={rightColumnRef}
+          className="col-md-6 d-flex align-items-center justify-content-center overflow-hidden"
+        >
           <div className="w-90 h-50 position-relative">
-            <div className='text-center w-75'>
+            <div className="text-center w-75">
               <img src={partyImage} alt="Party" className="phone-image" />
             </div>
           </div>
--- src/components/section3.js
+++ src/components/section3.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import '../css/section3.css';
 import installImage from '../images/install.png';
 import accountImage from '../images/account.png';
@@ -6,43 +6,90 @@ import drinkImage from '../images/drink.png';
 import funImage from '../images/fun.png';
 
 function Section3() {
+  const [fadeIn, setFadeIn] = useState([false, false, false, false]);
+  const [hasScrolledDown, setHasScrolledDown] = useState(false);
+  const sectionRef = useRef(null);
+
   const imageSize = '190px'; // Set the size for the images
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!hasScrolledDown) {
+        setHasScrolledDown(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [hasScrolledDown]);
+
+  useEffect(() => {
+    if (hasScrolledDown) {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const timeouts = [];
+            fadeIn.forEach((_, index) => {
+              timeouts.push(
+                setTimeout(() => {
+                  setFadeIn(prevState => {
+                    const newState = [...prevState];
+                    newState[index] = true;
+                    return newState;
+                  });
+                }, (index + 1) * 500) // Adjust timing as needed
+              );
+            });
+            observer.unobserve(sectionRef.current);
+          }
+        });
+      }, { threshold: 0.5 }); // Adjust threshold as needed
+
+      observer.observe(sectionRef.current);
+
+      return () => {
+        observer.disconnect();
+      };
+    }
+  }, [fadeIn, hasScrolledDown]);
   return (
-    <div className="section3 d-flex flex-column align-items-center justify-content-center pptt">
-      <h2 className="title3 pt-6">Guide</h2>
+    <div className="section3 d-flex flex-column align-items-center justify-content-center pptt" ref={sectionRef}>
+      <h2 className="title3 pt-6 fade-in">Guide</h2>
       <div className="row w-75 pptt2 ppbb">
-        <div className="col-md-3 col-sm-6 d-flex align-items-center justify-content-center">
+        <div className={`col-md-3 col-sm-6 d-flex align-items-center justify-content-center ${fadeIn[0] ? 'fade-in' : ''}`}>
           <div className="w-90 h-100 d-flex align-items-center justify-content-center">
             <div className='text-center w-100 '>
-            <p className='p311'>1</p>
-              <img src={installImage} alt="Install" style={{  height: imageSize ,height: imageSize, padding: '20px 0'}} />
+              <p className='p311'>1</p>
+              <img src={installImage} alt="Install" style={{ height: imageSize, padding: '20px 0' }} />
               <p className='p31'>Install the app</p>
             </div>
           </div>
         </div>
-        <div className="col-md-3 col-sm-6 d-flex align-items-center justify-content-center">
+        <div className={`col-md-3 col-sm-6 d-flex align-items-center justify-content-center ${fadeIn[1] ? 'fade-in' : ''}`}>
           <div className="w-90 h-100 d-flex align-items-center justify-content-center">
             <div className='text-center w-75'>
-            <p className='p321'>2</p>
+              <p className='p321'>2</p>
               <img src={accountImage} alt="Account" style={{ width: imageSize, height: imageSize }} />
               <p className='p32'>Sign up</p>
             </div>
           </div>
         </div>
-        <div className="col-md-3 col-sm-6 d-flex align-items-center justify-content-center">
+        <div className={`col-md-3 col-sm-6 d-flex align-items-center justify-content-center ${fadeIn[2] ? 'fade-in' : ''}`}>
           <div className="w-90 h-100 d-flex align-items-center justify-content-center">
             <div className='text-center w-75'>
-            <p className='p331'> 3</p>
+              <p className='p331'> 3</p>
               <img src={drinkImage} alt="Drink" style={{ width: imageSize, height: imageSize }} />
               <p className='p33'>Free drink</p>
             </div>
           </div>
         </div>
-        <div className="col-md-3 col-sm-6 d-flex align-items-center justify-content-center">
+        <div className={`col-md-3 col-sm-6 d-flex align-items-center justify-content-center ${fadeIn[3] ? 'fade-in' : ''}`}>
           <div className="w-90 h-100 d-flex align-items-center justify-content-center">
             <div className='text-center w-75'>
-            <p className='p341'>4</p>
+              <p className='p341'>4</p>
               <img src={funImage} alt="Fun" style={{ width: imageSize, height: imageSize }} />
               <p className='p34'>Have fun</p>
             </div>
