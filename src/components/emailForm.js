45bd5b81362da99a9181a74c19d6e585e1c59c2d
Jack Spinola <jackspinola198@hotmail.com>
2024-04-29 10:48:13 +0200

    subs

--- src/components/emailForm.js
+++ src/components/emailForm.js
@@ -1,48 +1,14 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React from 'react';
 import '../css/emailForm.css';
-import texts from '../text/texts.json'; 
+import Subscribe from './subscribe';
 
-const EmailForm = ({language}) => {
-    const [subject, setSubject] = useState('');
-    const [message, setMessage] = useState('');
-  
-    const handleSubmit = () => {
-      const recipient = 'oliver@pnta.dk';
-      const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
-      window.location.href = mailtoLink;
-    };
+const EmailForm = () => {
+
   
     return (
         <div className="emailSection py-5 ">
             <div className="email-form-container ">
-                <h2>{texts.emailFormTitle[language]}</h2>
-                <Form className="email-form">
-                <Form.Group controlId="subject">
-                    <Form.Label className="ttextt">{texts.emailFormSubjectLabel[language]}</Form.Label>
-                    <Form.Control
-                    type="text"
-                    placeholder={texts.emailFormSubjectPlaceholder[language]}
-                    value={subject}
-                    onChange={(e) => setSubject(e.target.value)}
-                    className="form-input"
-                    />
-                </Form.Group>
-                <Form.Group controlId="message">
-                    <Form.Label className="ttextt">{texts.emailFormMessageLabel[language]}</Form.Label>
-                    <Form.Control
-                    as="textarea"
-                    rows={5}
-                    placeholder={texts.emailFormMessagePlaceholder[language]}
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    className="form-input"
-                    />
-                </Form.Group>
-                <Button variant="primary" onClick={handleSubmit} className="submit-btn">
-                    {texts.emailFormSubmitButton[language]}
-                </Button>
-                </Form>
+                <Subscribe/>
             </div>
         </div>
     );
